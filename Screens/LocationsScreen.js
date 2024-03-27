bf8c06626968b292265f6e5779e344df07d27111
ki3mi <ccentemejiajosecarlos@gmail.com>
2024-03-26 22:01:17 -0500

    descripción

--- Screens/LocationsScreen.js
+++ Screens/LocationsScreen.js
@@ -11,35 +11,35 @@ const LocationScreen = () => {
             name: "Perú",
             precio: 1200,
             img: "https://www.peru.travel/contenido/acercadeperu/imagen/es/1/0.0/principal/machu%20picchu.jpg",
-            detalle: "texto de ejemplo largo para una descripción detallada y relleno"
+            detalle: "Perú, oficialmente la República del Perú, es un país soberano ubicado en la parte occidental de América del Sur. Su capital es Lima."
         },
         {
             id: 2,
             name: "Mexico",
             precio: 1000,
             img: "https://elcomercio.pe/resizer/ZWOVmzl0_-TfYS79fw5YEbzbgxc=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/CZGEWMPOLNCVNK2I3A6RNW63PQ.jpg",
-            detalle: "texto de ejemplo largo para una descripción detallada y relleno"
+            detalle: "México, oficialmente Estados Unidos Mexicanos, es un país situado en la parte meridional de América del Norte."
         },
         {
             id: 3,
             name: "Colombia",
             precio: 1100,
             img: "https://www.semana.com/resizer/JAPQOHfS7Qtfsq0dtu5z0hW0Y3k=/1280x0/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/BKALMBZ44JFANIN2VMGAD5XKI4.jpg",
-            detalle: "texto de ejemplo largo para una descripción detallada y relleno"
+            detalle: "Colombia, oficialmente República de Colombia, es un país soberano situado en la región noroccidental de América del Sur."
         },
         {
             id: 4,
             name: "Argentina",
             precio: 1300,
             img: "https://e.rpp-noticias.io/xlarge/2023/08/18/481648_1464895.webp",
-            detalle: "texto de ejemplo largo para una descripción detallada y relleno"
+            detalle: "Argentina, oficialmente República Argentina, es un país soberano de América del Sur, ubicado en el extremo sur y sudeste de dicho subcontinente."
         },
         {
             id: 5,
             name: "Uruguay",
             precio: 1400,
             img: "https://humanidades.com/wp-content/uploads/2018/07/uruguay-1-e1573265526917.jpg",
-            detalle: "texto de ejemplo largo para una descripción detallada y relleno"
+            detalle: "Uruguay, oficialmente República Oriental del Uruguay, es un país de América del Sur, situado en la parte oriental del Cono Sur americano."
         }
     ];
     const [selectedItem, setSelectedItem] = useState(null); // Estado para almacenar el elemento seleccionado
